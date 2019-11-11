9c8df07ffd6e5971474ab4b7dc95b7e3941d6ade
Julien Brunet <julienbrunet21@gmail.com>
2019-11-11 19:11:25 +0100

    improve

--- Functions.py
+++ Functions.py
@@ -4,154 +4,125 @@ from matplotlib import pyplot as plt
 import time
 from PIL.Image import *
 
+def detect_red(img):
+    """
+    This function is used to highlight red in image to easily detect circles
+    :param img: the image where we want to detect red (sign)
+    :return: img_threshold_red: the image segmented for red
+    """
+    img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert to HSV color-type
 
-def detectCircles(img):
+    lower_red = np.array([0, 120, 70])  # Range for lower red
+    upper_red = np.array([10, 255, 255])
+    mask1 = cv2.inRange(img_HSV, lower_red, upper_red)
+
+    lower_red = np.array([170, 120, 70])  # Range for upper red
+    upper_red = np.array([180, 255, 255])
+    mask2 = cv2.inRange(img_HSV, lower_red, upper_red)
+
+    mask1 = mask1 + mask2  # Generating the final mask
+
+    img_threshold_red = cv2.bitwise_and(img, img, mask=mask1)  # Detect and keep red
+
+    return img_threshold_red
+
+
+def detect_black(img):
+    """
+    This function is used to highlight black in image to easily detect number
+    :param img: the image where we want to detect black
+    :return: img_threshold_black: the image segmented for black
+    """
+    img_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert to HSV color-type
+
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([180, 255, 100])
+
+    mask = cv2.inRange(img_HSV, lower_black, upper_black)
+
+    return mask
+
+
+def detect_circles(img, original):
     """
     This function is used to detect the circles in an image
-    :param img: the image where we want to detect circles
+    :param img: the image where red has been highlighted and where we want to detect circles
+    :param original: the original image where circles will be drawn
     :return: result: an array of circles (x, y, r) if at least one circle has been found or FALSE if none circle has been found
-    :return: orginal: the orginal image with drawn circles to show what circles have been found
+    :return: original: the original image with drawn circles to show what circles have been found
     """
-
-    original = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to Gray color
-    img = cv2.medianBlur(img, 5)  # Apply filter to reduce false circles
-    cv2.imshow("test", img)
-    cv2.moveWindow("test",0,0)
-    rows = img.shape[0]
-    cols = img.shape[1]
-
     result = []
-    sizeMax = 101 # Size max of the Radius
+    size_max = 100                                                           # Size max of the Radius
     n = 0
+    found = 0
 
-    while (n < sizeMax):
-        max = sizeMax - n  # Use standard ratio size of traffic sign
-        min = int(0.65 * max)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                             # Convert to Gray color
+    img = cv2.medianBlur(img, 5)                                            # Apply filter to reduce false circles
+    rows = img.shape[0]
 
-        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, rows / 8, param1=100, param2=40, minRadius=min,
-                                   maxRadius=max)
+    while n < size_max:
+        max = size_max - n                                                   # Use standard ratio size of traffic sign
+        min = int(0.7 * max)
 
-        if circles is not None:  # If circle has been found
+        circles = cv2.HoughCircles(                                         # Detect circles thx to Hough Method
+            img,
+            cv2.HOUGH_GRADIENT,
+            1,
+            rows/4,
+            param1=200,
+            param2=20,
+            minRadius=min,
+            maxRadius=max)
+
+        if circles is not None:                                             # If at least one circle has been found
             circles = np.round(circles[0, :]).astype("int")
             for elt in circles:
-                result.append(elt)  # Add the circle found to an array
+                result.append(elt)                                          # Add the circle found to an array
             for j in range(np.shape(result)[0]):
                 x = result[j][0]
                 y = result[j][1]
                 r = result[j][2]
                 h = r
                 w = r
-                img[y - h:y + h, x - w:x + w] = 0  # Replace the circle by a black square
-
+                img[y - h:y + h, x - w:x + w] = 255                          # Replace the circle by a black square
         n = n + 5
 
     if result is not None:
-        for j in range(np.shape(result)[0]):
+        found = np.shape(result)[0]                                          # Number of circles found
+        for j in range(found):
             x = result[j][0]
             y = result[j][1]
             r = result[j][2]
             cv2.circle(original, (x, y), r, (0, 255, 0), 4)
-            cv2.rectangle(original, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            #cv2.rectangle(original, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
-        return result, original
-
-    return False, original
+    return found, result, original
 
 
-def extractCirclesAfterDetection(img, circles):
+def crop(img, circles):
     """
-    This function is used to crop the circles one they have been detected
+    This function is used to crop the circles one they have been detected, the goal is to keep only the number inside the circle
     :param img: original image
     :param circles: array of circles (x, y, r) which have been detected in the orginal image
-    :return: croped: array of images which have been extracted thanks to the array of circles
+    :return: extracted: array of images which have been extracted thanks to the array of circles
     """
     extracted = []
     print circles
     if circles is not None:
         for (x, y, r) in circles:
+            #margin = int(r / 4)
+            margin = -int(r/3)
             h = r
             w = r
-            temp = img[y - h: y + h, x - w: x + w]
+            temp = img[y - h - margin: y + h + margin, x - w - margin: x + w + margin]
             temp = cv2.resize(temp, (100, 100), interpolation=cv2.INTER_LANCZOS4)
             #cv2.imwrite('assets/temp.png', temp)
 
             extracted.append(temp)
     return extracted
 
-def cropCirclesCanny(img):
-    """
-    This function is used to crop the image more precisely and keep only the number if the image is a traffic sign and then apply the canny filter
-    :param img: the image containing the circle which has been extracted thanks to extractCirclesAfterDetection()
-    :return: final: the croped image
-    """
-    temp = img[25:75, 25:50]  # half of the image
-    final = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-    final = cv2.Canny(final, 0, 200)
-
-    #cv2.imwrite('assets/ImageFinal.png', final)
-    cv2.imshow('Canny croped',final)
-    cv2.moveWindow('Canny croped', 0, 0)
-
-    return final
-
-
-
-
-
-def detectRed(img):
-    """
-    This function is used to detect the red color on an image and decide if the image is a sign or not, it enable to
-    eliminate false positives found by detectCircles
-    :param img: the image croped by extractCirclesAfterDetection
-    :return: imgThresholdr: the image with the red highlighted
-    :return: sign: TRUE if the image is a sign or FALSE if not
-    """
-    sign = False
-
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Convert to HSV color-type
-
-    # hue_hist = cv2.calcHist([imgHSV], [0], None, [180], [0, 180])  # Find the histogram for the hue band
-    # plt.plot(hue_hist)
-    # plt.xticks(np.arange(0, 190, step=10))  # Config the x axis for better view
-    # plt.show()
-
-    h, s, v = cv2.split(imgHSV);  # Split to work only on h (hue)
-
-    redthreshold = np.where(((h >= 175) & (h <= 180)) | ((h >= 0) & (h <= 5)))
-
-    imgThresholdr1 = np.zeros((np.shape(img)[0], np.shape(img)[1]))
-    imgThresholdr2 = np.zeros((np.shape(img)[0], np.shape(img)[1]))
-    imgThresholdr3 = np.zeros((np.shape(img)[0], np.shape(img)[1]))
-
-    imgThresholdr1[redthreshold] = h[redthreshold]
-    imgThresholdr2[redthreshold] = s[redthreshold]
-    imgThresholdr3[redthreshold] = v[redthreshold]
-
-    imgThresholdr = imgHSV
-
-    imgThresholdr[:, :, 0] = imgThresholdr1
-    imgThresholdr[:, :, 1] = imgThresholdr2
-    imgThresholdr[:, :, 2] = imgThresholdr3
-    cv2.imshow(",,", imgThresholdr)
-    imgThresholdr = cv2.cvtColor(imgThresholdr, cv2.COLOR_HSV2BGR)
-
-
-    nbred = np.shape(redthreshold)[1]
-    nbtotal = np.shape(h)[0] * np.shape(h)[0]
-    ratio = float(nbred) / float(nbtotal)
-
-    print ratio
-
-    if ratio > 0.25:
-        sign = True
-
-
-    return imgThresholdr, True
-
-
 # Image.putpixel(i, (x, y), 255)
-def whitePixels(img):
+def white_pixels(img):
     """
     This function is used to count the number of white pixels
     :param img: the image with canny filter
@@ -161,74 +132,75 @@ def whitePixels(img):
     (l, h) = np.shape(img)
     for y in range(h):
         for x in range(l):
-            if (img[x,y] == 255):
+            if img[x, y] == 255:
                 n = n + 1
     print"nombre de pixels blanc = ", n
     return n
 
 
-def detectNumber(img):
+def detect_number(img):
     """
     This function is used to detect the number is the traffic sign
     :param img: the img with canny filter
     :return:
     """
-    nbWhite = whitePixels(img)
+    nb_white = white_pixels(img)
     #nbTotal = np.shape(img)[0] * np.shape(img)[0]
     #ratio = float(nbWhite) / float(nbTotal)
-    validation(nbWhite)
+    validation(nb_white)
 
 
-
-
-
-
-def validation(nbWhite):
-    if (nbWhite > 175 and nbWhite < 180):
+def validation(nb_white):
+    if (nb_white > 175 and nb_white < 180):
         panneau = 30
         print "La limitation est de                         :" , panneau
 
-    if (nbWhite > 123 and nbWhite < 143):
+    if (nb_white > 123 and nb_white < 143):
         panneau = 70
         print "La limitation est de                         :" , panneau
 
-    if (nbWhite > 161 and nbWhite < 165):
+    if (nb_white > 161 and nb_white < 165):
         panneau = 50
         print "La limitation est de                         :" , panneau
 
 
 
-def Algorithm(img):
+def algorithm(img):
     """
     Function used to launch the algorithm
     """
-    # cv2.imshow("Img", img)
-    c, d = detectCircles(img)                                               # Detect circles in the image
+    cv2.imshow("Image", img)
+    cv2.moveWindow("Image", 0, 0)
+    original = img.copy()
 
-    cv2.imshow("Circles detected", d)
+    # First highlight red in the image
+    img_red = detect_red(img)
+    cv2.imshow("Red segmentation", img_red)
+    cv2.moveWindow("Red segmentation", 1000, 0)
+
+    # Then detect circles
+    found, circles, drawn = detect_circles(img_red, img)
+    cv2.imshow("Circles detected", img)
     cv2.moveWindow('Circles detected', 0, 0)
 
-    extracted = extractCirclesAfterDetection(img, c)                        # Extract the detected circles
+    # Then extract the part of the image where circles has been detected
+    if found>0:
+        extracted = crop(original, circles)              # Extract the detected circles
+        for j in range(np.shape(extracted)[0]):                                     # Loop through the array containing the extracted image
+            if extracted[j] is not None:
+                cv2.imshow("Extracted" + str(j), extracted[j])                      # Show the interesting part
+                cv2.moveWindow("Extracted" + str(j), 0, 0)
 
+                img_black = detect_black(extracted[j])
+                cv2.imshow("Black segmentation on extracted " + str(j), img_black)
+                cv2.moveWindow("Black segmentation on extracted " + str(j), 0, 0)
 
-    for j in range(np.shape(extracted)[0]):                                 # Loop through the array containin the extracted image
-        if extracted[j] is not None:
-            cv2.imshow("Croped " + str(j), extracted[j])
-            cv2.moveWindow('Croped', 0, 0)
-            colored, sign = detectRed(extracted[j])                         # Detect presence of red in the extracted image
-
-            print "Image is a traffic sign: "+str(sign)
-
-            if sign == True:                                                # If we are sure than the image is traffic sign
-                cv2.imshow("Croped colored" + str(j), colored)
-                cv2.moveWindow("Croped colored", 0, 0)
-                ImageCropCanny = cropCirclesCanny(extracted[j])             # Crop to keep the number and apply canny filter
-                detectNumber(ImageCropCanny)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     print("END")
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
     print "\n"
+
 
 
 
--- Main.py
+++ Main.py
@@ -4,48 +4,27 @@ import time
 from PIL.Image import *
 import cv2
 import os
-"""
-nombreDeBoucles=0
-a = 0
-i=0
-securite=1
-"""
 
-cap = cv2.VideoCapture(0)
+
+
 """
+cap = cv2.VideoCapture("assets/video1.avi")
+#cap = cv2.VideoCapture(0)
+
 while True:
     
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, 1)
-    cv2.imwrite('assets/hsv_frame.png', hsv_frame)
-    hsv_frame = cv2.imread('assets/hsv_frame.png', -1)
-    Functions.Algorithm(hsv_frame)
-    
-    
-    OLD ALGO : SEE FUNCTIONS
-    if(Functions.selection(frame)=="valide"):
-       i=i+1
-       finalpixels = open("assets/ImageFinal.png")          # permet de calculer le nombre de pixel moyen blanc pour le chiffre de gauche
-       NombrePixel=Functions.whitePixels(finalpixels)
-       a=a+NombrePixel
-       nombreDeBoucles = nombreDeBoucles + 1
-       NombrePixelMoyenne=a/nombreDeBoucles
-       print "le nombre de pixel moyen est" , NombrePixelMoyenne
-       if i==securite:
-          Functions.validation(NombrePixelMoyenne)
-          i=0
-    else:
-        Noir=cv2.imread('assets/Noir.png', 1)
-        cv2.imwrite('assets/ImageFinal.png',Noir)
-        nombreDeBoucles=0
-        a=0
-
+    cv2.imshow("", hsv_frame)
+    #cv2.imwrite('assets/hsv_frame.png', hsv_frame)
+    #hsv_frame = cv2.imread('assets/hsv_frame.png', -1)
+    Functions.algorithm(hsv_frame)
 
     key = cv2.waitKey(1)
     if key == 27:
         break
-
 """
+
 
 #### Test with some images ####
 Itraffic1 = cv2.imread("assets/Traffic1.jpg", -1)
@@ -66,19 +45,19 @@ ref30 = cv2.imread("assets/ref30.jpg", -1)
 
 
 
-Functions.Algorithm(Itraffic1)
-Functions.Algorithm(Itraffic2)
-Functions.Algorithm(Itraffic3)
-Functions.Algorithm(Itraffic4)
-Functions.Algorithm(Itraffic5)
-Functions.Algorithm(Itraffic6)
-Functions.Algorithm(Itraffic7)
-Functions.Algorithm(Itraffic8)
-Functions.Algorithm(Itraffic9)
-Functions.Algorithm(Itraffic10)
-Functions.Algorithm(Itraffic11)
-Functions.Algorithm(Itraffic12)
-Functions.Algorithm(Itraffic13)
+Functions.algorithm(Itraffic1)
+Functions.algorithm(Itraffic2)
+Functions.algorithm(Itraffic3)
+Functions.algorithm(Itraffic4)
+Functions.algorithm(Itraffic5)
+Functions.algorithm(Itraffic6)
+Functions.algorithm(Itraffic7)
+Functions.algorithm(Itraffic8)
+Functions.algorithm(Itraffic9)
+Functions.algorithm(Itraffic10)
+Functions.algorithm(Itraffic11)
+Functions.algorithm(Itraffic12)
+Functions.algorithm(Itraffic13)
 
 
 ## END ##
