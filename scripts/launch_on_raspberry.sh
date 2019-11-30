0a25962ee679d2a950e08c9b4d3e5bb22eb1d07c
Julien Brunet <julienbrunet21@gmail.com>
2019-11-30 17:14:18 +0100

    misc

--- scripts/launch_on_raspberry.sh
+++ scripts/launch_on_raspberry.sh
@@ -1,3 +1,4 @@
+cd ../
 git pull
 sudo find / -type f -name ‘atom.so*’ /usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0
 LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0 python3 MainRaspberry.py
