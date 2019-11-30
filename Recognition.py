00799b07ca6c0828df310d4e73ae4913cfd5f986
Julien Brunet <julienbrunet21@gmail.com>
2019-11-30 18:45:57 +0100

    add multiplatform for tesseract

--- Recognition.py
+++ Recognition.py
@@ -1,18 +1,16 @@
 import pytesseract
 from PIL import Image
+from sys import platform as _platform
 
 
-## FOR MAC OS ##
-pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/4.1.0/bin/tesseract"
-
-"""
-## FOR WINDOWS ##
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-
-## FOR RASPIAN ##
-pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
-"""
+if _platform == "linux" or _platform == "linux2":
+    pytesseract.pytesseract.tesseract_cmd = r"/usr/bin/tesseract"
+elif _platform == "darwin":
+    pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/4.1.0/bin/tesseract"
+elif _platform == "win32":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+elif _platform == "win64":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def detect_number(img):
     image = Image.fromarray(img)
--- Tests.py
+++ Tests.py
@@ -13,7 +13,6 @@ import PreProcessing
 import Recognition
 
 
-
 #cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("assets/vid1.mov")
 #cap = cv2.VideoCapture("assets/test.mp4")
