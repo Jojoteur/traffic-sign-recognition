c3c410253c4c9fdb1f45f21954deba91df1a51eb
Julien Brunet <julienbrunet21@gmail.com>
2019-12-05 18:52:30 +0100

    improve detection

--- PreProcessing.py
+++ PreProcessing.py
@@ -176,7 +176,7 @@ def pre_processing(img):
                 #cv2.moveWindow("Black segmentation on extracted " + str(j), 0, 0)
 
 
-                img_black = improve(img_black)
+                #img_black = improve(img_black)
                 #cv2.imshow("Improve after black semgentation" + str(j), img_black)
                 #cv2.moveWindow("Improve after black semgentation"  + str(j), 0, 0)
                 #cv2.imwrite("assets/img" + str(j) +".png", img_black)
