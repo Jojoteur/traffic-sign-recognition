9353b0c99879534a9d8e8558bab4c5c0179f3428
Julien Brunet <julienbrunet21@gmail.com>
2020-02-01 18:04:43 +0100

    add end of speed limits detection

--- Processing.py
+++ Processing.py
@@ -231,7 +231,7 @@ def end_extractor(img):
     """
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     mask = np.zeros_like(imgray)
 
